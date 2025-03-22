dff171bc17420a702b07b9698a0eddd28fefe47d
alifbintang1 <alifbintange@gmail.com>
2025-03-23 01:03:36 +0700

    implement recursive and backtrack

--- solver.py
+++ solver.py
@@ -7,5 +7,11 @@ if __name__ == "__main__":
     parser.add_argument("-f", type=str, required=True, help="Path to the CNF file")
     args = parser.parse_args()
 
-    SOLVER = RSSolver(args.f)
-    print(SOLVER.solve())
+    solver = RSSolver(args.f)  # Replace with your CNF file path
+    solution = solver.solve()
+    print("Solution:", solution)
+
+    is_valid = solver.validate()
+    print("Is solution valid?", is_valid)
+
+    # print(SOLVER.validate()) #debug
