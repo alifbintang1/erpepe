ea89843839dac4eb5d781f552460e2c05cf40045
Yosef <yosef.nuraga@ui.ac.id>
2025-03-23 01:00:29 +0700

    Feat: add true dataset

--- utils/Solver.py
+++ utils/Solver.py
@@ -1,24 +1,28 @@
 from typing import List, Tuple
-from utils import Parser
+from utils.Parser import Parser
+from tqdm import tqdm
 
 class RSSolver:
-    def __init__(self, file_path: str):
+    def __init__(self, file_path: str, verbose: bool = False):
         self.parser = Parser(file_path)
         self.T = []
         self.res = [False for x in range(self.parser.num_clauses)]
+        self.verbose = verbose
 
     def solve(self)-> bool:
-        for i in range(1,self.parser.num_vars+1):
+        for i in tqdm(range(1, self.parser.num_vars + 1), desc="Processing Variables",disable = not self.verbose):
             self.T.append(i)
             if self.__validate__():
-                self.T[-1] = -self.T[-1] 
+                self.T[-1] = -self.T[-1]
 
-        for i,clause in enumerate(self.parser.data):
+        # Second loop with progress bar
+        for i, clause in tqdm(enumerate(self.parser.data), total=len(self.parser.data), desc="Processing Clauses", disable = not self.verbose):
             if any(var in self.T for var in clause):
                 self.res[i] = True
         
         return self.res #debug
         # return all(self.res)
+
 
     def __validate__(self)->bool:
         for clause in self.parser.data:
